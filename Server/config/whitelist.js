2cc303cbec265c60f866baa658cfcb5f5299617f
Vara Lakshmaiah <105189764+Varalakshmaiah28@users.noreply.github.com>
2024-02-12 14:40:56 +0530

    Update whitelist.js

--- Server/config/whitelist.js
+++ Server/config/whitelist.js
@@ -2,7 +2,7 @@ const whitelist = [
     'https://www.google.com',
     'http://localhost:3500',
     'http://localhost:3000',
-    'https://final-year-project-client-five.vercel.app/'
+    'https://final-year-project-client-five.vercel.app'
 ];
 
 module.exports = whitelist;
