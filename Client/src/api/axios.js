b308f50fb2285d74f634dec054ab1639ada18125
Dharani Mahesh <mahesh.ddm7@gmail.com>
2024-03-02 16:42:45 +0530

    cors fix 2

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -2,11 +2,16 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: 'https://final-year-project-server.vercel.app/',
-    withCredentials: true
+    // baseURL: 'http://localhost:3500/',
+    // withCredentials: true
 });
 
 export const axiosPrivate = axios.create({
     baseURL: 'https://final-year-project-server.vercel.app/',
-    headers: { 'Content-Type': 'application/json' },
-    withCredentials: true
+    // baseURL: 'http://localhost:3500/',
+    headers:{ 
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS" 
+            },
+    // withCredentials: true
 });
--- Server/Server.js
+++ Server/Server.js
@@ -14,20 +14,20 @@ connectDB();
 
 app.use(credentials);
 
-app.use(cors(corsOptions));
-app.use((req, res, next) => {
-    const allowedOrigins = ['https://final-year-project-client-five.vercel.app', 'http://localhost:3000'];
-    const origin = req.headers.origin;
+app.use(cors());
+// app.use((req, res, next) => {
+//     const allowedOrigins = ['https://final-year-project-client-five.vercel.app', 'http://localhost:3000'];
+//     const origin = req.headers.origin;
   
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
+//     if (allowedOrigins.includes(origin)) {
+//       res.setHeader('Access-Control-Allow-Origin', origin);
+//     }
   
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
-    next();
-  });
+//     next();
+//   });s
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
