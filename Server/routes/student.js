cd9ddaef00b6b93790aabd66e0d4b85119ef10f4
Dharani Mahesh <mahesh.ddm7@gmail.com>
2024-03-02 17:08:26 +0530

    CORS fix - 8

--- Server/routes/student.js
+++ Server/routes/student.js
@@ -7,6 +7,14 @@ const { profileUpload, resumeUpload } = require('../middleware/upload');
 
 router.use(verifyRole('student'));
 
+router.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+});
+
 router.get('/$', studentController.getDashboard);
 
 router.get('/details$', studentController.getStudent);
